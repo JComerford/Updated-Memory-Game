2c530596545616c4698b8c7f141e5e01effee03c
Justin Comerford <comerford.jac@gmail.com>
2018-08-05 17:43:50 -0400

    functions for addToOpenCards/flipCard + comments

--- js/app.js
+++ js/app.js
@@ -34,14 +34,15 @@ function shuffle(array) {
 }
 
 //loop through each card and create its HTML
+
+//set up the event listener for a card.
 cards.forEach(card => {
-    card.addEventListener('click', (e => {
-        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-            openCards.push(card);
-            card.classList.add('open', 'show');
-            console.log(openCards);
+    card.addEventListener('click', (e => { //if card is clicked: Check conditions:
+        if (openCards.length < 2 && !card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) { 
+            addToOpenCards(card); // add card to list of open cards
+            flipCard(card); // show image on card          
         }  
-        if (openCards.length == 2) {
+        if (openCards.length === 2) {
             setTimeout(function() {
                 openCards.forEach(card => {
                     card.classList.remove('open', 'show');
@@ -55,20 +56,20 @@ cards.forEach(card => {
  /*   - add each card's HTML to the page
  */
 
-/*function flipCard() {
-    openCards.push(card);
+ //display the card's symbol
+function flipCard(card) {
     card.classList.add('open', 'show');
     console.log(openCards);
 }
-*/
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- */
- //function match() {
-//}
+
+//adds to NodeList
+function addToOpenCards(card) {
+    openCards.push(card);
+}
+
+// if the list already has another card, check to see if the two cards match
+ 
+
  /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
