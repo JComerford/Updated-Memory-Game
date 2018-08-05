0a67bb3c5df029e3bbd91e086f17082c40d10c41
Justin Comerford <comerford.jac@gmail.com>
2018-08-05 17:10:28 -0400

    cards flipping with conditions

--- js/app.js
+++ js/app.js
@@ -36,27 +36,39 @@ function shuffle(array) {
 //loop through each card and create its HTML
 cards.forEach(card => {
     card.addEventListener('click', (e => {
-        if (openCards.length < 2) {
+        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
             openCards.push(card);
             card.classList.add('open', 'show');
             console.log(openCards);
-        } 
-
-    }))
-})
+        }  
+        if (openCards.length == 2) {
+            setTimeout(function() {
+                openCards.forEach(card => {
+                    card.classList.remove('open', 'show');
+                });
+                openCards = [];
+            }, 1000);
+         } 
+    }));
+});
 
  /*   - add each card's HTML to the page
  */
 
-
-
+/*function flipCard() {
+    openCards.push(card);
+    card.classList.add('open', 'show');
+    console.log(openCards);
+}
+*/
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  */
- 
+ //function match() {
+//}
  /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
