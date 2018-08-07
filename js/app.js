22eaf86206ab5a516ec693a234c58f7f775a71bc
Justin Comerford <comerford.jac@gmail.com>
2018-08-06 23:05:32 -0400

    Added modal info, bugs on game reset

--- js/app.js
+++ js/app.js
@@ -27,7 +27,8 @@ function initGame() {
         return generateCard(card);
     });
     deck.innerHTML = cardHTML.join('');
-    checkScore(); 
+    checkScore();
+
 }
 initGame(); //add each card's HTML to the page
 
@@ -43,6 +44,8 @@ let stars = document.querySelectorAll('.stars li'); //selects all with stars in 
 let clockOff = true;
 let time = 0;
 let clockId;
+let matched = 0;
+let allMatched = 8;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -59,15 +62,16 @@ function shuffle(array) {
 }
 
 //set up the event listener for a card. Event listener created for each card.
-cards.forEach(card => {
-    card.addEventListener('click', (e => { //if card is clicked:
-        if (canCardBeClicked(card)) { //check conditional for click
-            addToOpenCards(card); // add card to list of open cards
-            flipCard(card); // show image on card  
-            matchMaker(card); //check cards for match. If match, stay flipped. If not, flip over.
-        }    
-    }));
-});
+    cards.forEach(card => {
+        card.addEventListener('click', (e => { //if card is clicked:
+            if (canCardBeClicked(card)) { //check conditional for click
+                addToOpenCards(card); // add card to list of open cards
+                flipCard(card); // show image on card  
+                matchMaker(card); //check cards for match. If match, stay flipped. If not, flip over.
+            }
+        }));
+    });
+
 
 //Set conditional for card to be clicked.
  function canCardBeClicked(card) {
@@ -102,8 +106,12 @@ function matchMaker() { //if the cards do match, lock the cards in the open posi
             openCards[0].classList.add('match');
             openCards[1].classList.add('match');
             openCards = [];
+            matched++;
             turn();
             checkScore();
+            console.log(`Matched = ${matched}`); //for debugging
+        } if (matched === allMatched) {
+            gameOver();
         } else {
             flipBack(); // if the cards do not match, remove the cards from the list and hide the card's symbol
             turn();
@@ -130,6 +138,7 @@ function checkScore() {
 
 //Counting Stars
 function removeStar() {
+    const stars = document.querySelectorAll('.stars li');
     for (star of stars) {
         if (star.style.display !== 'none') {
             star.style.display = 'none';
@@ -139,6 +148,7 @@ function removeStar() {
 }
 removeStar();
 removeStar();
+
 //    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  
 
@@ -148,7 +158,7 @@ function startClock() {
         clockId = setInterval(() => {
             time++;
             displayTime();
-            console.log(time);
+            //console.log(time); //for debugging
         }, 1000);
     }
 
@@ -167,7 +177,7 @@ function stopClock() {
 //Displays time in browser. Gives both minutes and seconds.
 function displayTime() {
     const clock = document.querySelector('.clock');
-    console.log(clock);
+    //console.log(clock); // for debugging
     clock.innerHTML = time;
     const seconds = time % 60;
     const minutes = Math.floor(time / 60);
@@ -177,3 +187,107 @@ function displayTime() {
         clock.innerHTML = `Time ${minutes}:${seconds}`;
     }
 }
+
+//dealing with modal when user finishes game: pop-up.
+function toggleModal() {
+    const modal = document.querySelector('.modal-background'); //sets variable to toggle.
+    modal.classList.toggle('hide'); //toggle on/off, hide/un-hide
+}
+toggleModal(); //Calls to open modal
+toggleModal(); //calls to hide modal
+
+//For testing Modal
+/*time = 121;
+displayTime(); //2:01
+moves = 16;
+checkScore(); //2 stars
+
+writeModalStats();
+toggleModal(); //open modal
+*/
+function writeModalStats() {
+    const timeStat = document.querySelector('.modal-time');
+    const clockTime = document.querySelector('.clock').innerHTML;
+    const movesStat = document.querySelector('.modal-moves');
+    const starsStat = document.querySelector('.modal-stars');
+    const stars = getStars();
+
+    timeStat.innerHTML = `${clockTime}`;
+    movesStat.innerHTML = `Moves ${moves}`;
+    starsStat.innerHTML = `Stars ${stars}`;
+}
+
+function getStars() {
+    stars = document.querySelectorAll('.stars li');
+    starCount = 0;
+    for (star of stars) {
+        if (star.style.display !== 'none') {
+            starCount++;
+        }
+    }
+    console.log(`Stars = ${starCount}`); //2
+    return starCount;
+}
+
+//modal button functionality
+//Cancel-button:
+document.querySelector('.modal-cancel').addEventListener('click', toggleModal);
+
+
+//Replay Button:
+document.querySelector('.modal-replay').addEventListener('click', replayButton);
+
+
+//Giving reset button functionality.
+function resetStats() {
+    stopClock(); //stops clock
+    clockOff = true;
+    time = 0;
+    displayTime(); //resets clock
+    matched = 0; //sets matched pairs count back to 0 from 8.
+    moves = 0; //resets moves
+    document.querySelector('.moves').innerHTML = moves;
+
+    //stars = 0;
+    const stars = document.querySelectorAll('.stars li');
+    for (star of stars) {
+        star.style.display = 'inline';
+    }
+}
+
+//Resets game & shuffles deck.
+function gameReset() {
+    initGame();
+    resetStats();           //Clock works, but cards are not clickable...
+
+    //resetCards();
+    //checkScore();
+
+    
+}
+
+function replayButton() {
+    gameReset();
+    toggleModal();
+    //resetCards();         //Clock works, but cards are not clickable...
+    resetStats();
+
+}
+//Functionality for "Reset" Button:
+document.querySelector('.restart').addEventListener('click', gameReset);
+
+//reset game after win
+function resetCards() {                                     //event listener is only on the deck, but not on individual cards after reset...
+    const cards = document.querySelectorAll('.deck li');   //TODO: Do I need this???? initGame resets all but eventlistener on cards.  Clock works though.
+    for (let card of cards) {
+        card.className = 'card';
+    }
+}
+
+//End of Game
+function gameOver() {
+    stopClock();
+    writeModalStats();
+    toggleModal();
+}
+
